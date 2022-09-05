a1dff8ff5d77e008d632134f76e3f3e8a9ab9e59
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-05 07:32:21 +0300

    I think I solved it

--- app.py
+++ app.py
@@ -220,17 +220,17 @@ colors = ['cyan', 'brown', 'white', 'red', 'green', 'blue', 'yellow', 'purple']
 
 for i, orient in enumerate(orientations):
     faces = face_dict[orient]
-    st.write("faces", faces)
-    clus = topologic.Cluster.ByTopologies(faces)
-    st.write("Cluster", clus)
-    mesh_data = pvMeshByTopology(topology=clus)
-    mesh_data.clean()
-    p.add_mesh(mesh_data, color=colors[i], specular=1.0, specular_power=10, show_edges=True, opacity=mesh_opacity, lighting=True)
-    apertures = aperture_dict[orient]
-    clus = topologic.Cluster.ByTopologies(apertures)
-    mesh_data = pvMeshByTopology(topology=clus)
-    mesh_data.clean()
-    p.add_mesh(mesh_data, color=colors[i], specular=1.0, specular_power=10, show_edges=True, opacity=mesh_opacity, lighting=True)
+    if len(faces) > 0:
+        clus = topologic.Cluster.ByTopologies(faces)
+        mesh_data = pvMeshByTopology(topology=clus)
+        mesh_data.clean()
+        p.add_mesh(mesh_data, color=colors[i], specular=1.0, specular_power=10, show_edges=True, opacity=mesh_opacity, lighting=True)
+        apertures = aperture_dict[orient]
+        if len(apertures) > 0:
+            clus = topologic.Cluster.ByTopologies(apertures)
+            mesh_data = pvMeshByTopology(topology=clus)
+            mesh_data.clean()
+            p.add_mesh(mesh_data, color=colors[i], specular=1.0, specular_power=10, show_edges=True, opacity=mesh_opacity, lighting=True)
 
 with tab1:
     # Draw the 3D view in tab 1
